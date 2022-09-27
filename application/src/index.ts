5567821e1a119e3d3b8030f4f00813a7b3ec6fe3
noel garcia <tx.swengmg@obs.tv>
2022-09-27 18:38:12 +0100

    added prometheus client

--- application/src/index.ts
+++ application/src/index.ts
@@ -1,9 +1,18 @@
 import * as express from 'express';
 import 'express-async-errors';
+import * as promBundle from 'express-prom-bundle';
+
 
 const app = express();
+const metricsMiddleware = promBundle({
+  buckets: [0.1, 0.4, 0.7],
+  includeMethod: true,
+  includePath: true
+});
 
-app.get('*', (req: express.Request, res: express.Response) => {
+app.use(metricsMiddleware);
+
+app.get('/', (req: express.Request, res: express.Response) => {
   const response = {
     hostname: req.hostname,
     uptime: process.uptime(),
@@ -12,6 +21,7 @@ app.get('*', (req: express.Request, res: express.Response) => {
 
   res.status(200).send(response);
 });
+
 
 app.listen(3000, () => {
   console.log('listening on 3000');
